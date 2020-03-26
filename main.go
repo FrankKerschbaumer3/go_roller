ccc141dd1e6df723d9450915226e864c8f2ddf99
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2020-03-26 12:47:18 -0400

    Split logic of min max into a function

--- main.go
+++ main.go
@@ -7,12 +7,29 @@ import (
 	"time"
 )
 
+// MinMax creates a min and max value.
+func MinMax(a []int) (int, int) {
+	min := a[0]
+	max := a[0]
+	for _, value := range a {
+		if max < value {
+			max = value
+		}
+		if min > value {
+			min = value
+		}
+	}
+	return min, max
+}
+
+//Function to roll dice
 func main() {
 
+	//Flags to parse dice conditions
 	diceToRoll := flag.Int("dicetoroll", 1, "What dice will you roll?")
 	amountOfDice := flag.Int("amountofdice", 1, "How many dice will you roll?")
 	atAdvantage := flag.Bool("advantage", false, "Roll with Advantage?")
-	atDisadvantage := flag.Bool("disadvantage", false, "Roll with Disdvantage?")
+	atDisadvantage := flag.Bool("disadvantage", false, "Roll with Disadvantage?")
 	modifier := flag.Int("modifier", 0, "What is your modifier?")
 	flag.Parse()
 
@@ -20,20 +37,12 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	if *atAdvantage == true {
-
 		for i := 1; i <= *amountOfDice; i++ {
 			num := 1 + rand.Intn(*diceToRoll)
 			slice = append(slice, num)
 		}
-		max := slice[0]
+		_, max := MinMax(slice)
 
-		for i := 1; i < *amountOfDice; i++ {
-
-			if max < slice[i] {
-
-				max = slice[i]
-			}
-		}
 		fmt.Println(*modifier + max)
 	}
 
@@ -42,15 +51,8 @@ func main() {
 			num := 1 + rand.Intn(*diceToRoll)
 			slice = append(slice, num)
 		}
-		min := slice[0]
 
-		for i := 1; i < *amountOfDice; i++ {
-
-			if min > slice[i] {
-
-				min = slice[i]
-			}
-		}
+		min, _ := MinMax(slice)
 		fmt.Println(*modifier + min)
 	}
 
