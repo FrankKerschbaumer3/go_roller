fc2e4205f29b5b10a6afab64c43de814e7f69656
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2020-03-30 19:27:42 -0400

    Create test for min max function

---
+++ main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+var minrolls = []struct {
+	in       []int
+	expected int
+}{
+	{[]int{1, 13}, 1},
+	{[]int{7, 3}, 3},
+	{[]int{4, 26}, 4},
+	{[]int{18, 12, 22, 7}, 7},
+	{[]int{3}, 3},
+}
+
+var maxrolls = []struct {
+	in       []int
+	expected int
+}{
+	{[]int{1, 13}, 13},
+	{[]int{7, 3}, 7},
+	{[]int{4, 26}, 26},
+	{[]int{18, 12, 22, 7}, 22},
+	{[]int{3}, 3},
+}
+
+func TestMin(t *testing.T) {
+	for _, rolls := range minrolls {
+		t.Run("Min", func(t *testing.T) {
+			result := Min(rolls.in)
+			if result != rolls.expected {
+				t.Error("Something went wrong :(")
+			}
+		})
+	}
+}
+
+func TestMax(t *testing.T) {
+	for _, rolls := range maxrolls {
+		t.Run("Max", func(t *testing.T) {
+			result := Max(rolls.in)
+			if result != rolls.expected {
+				t.Error("Something went wrong :(")
+			}
+		})
+	}
+}
