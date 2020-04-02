586d58da043b47d7bc013841f2bab3ac5ee9dbb5
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2020-04-01 22:21:34 -0400

    Move rand seed to init function

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	crypto_rand "crypto/rand"
+	"encoding/binary"
 	"flag"
 	"fmt"
-	"math/rand"
-	"time"
+	math_rand "math/rand"
 
 	"github.com/FrankKerschbaumer3/go_roller/roll"
 )
 
+// Creates seed to have random dice rolls
+func init() {
+	var b [8]byte
+	_, err := crypto_rand.Read(b[:])
+	if err != nil {
+		panic("cannot seed math/rand package with cryptographically secure random number generator")
+	}
+	math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
+}
+
 //Function that will accept ints and booleans to output a dice total.
 func main() {
 
-	//Flags to parse dice conditions
+	//would call function to create gui
+	//gui would take in user inputs and return inputs/flags here
+
 	diceToRoll := flag.Int("dicetoroll", 1, "What dice will you roll?")
 	amountOfDice := flag.Int("amountofdice", 1, "How many dice will you roll?")
 	atAdvantage := flag.Bool("advantage", false, "Roll with Advantage?")
@@ -21,9 +34,10 @@ func main() {
 
 	flag.Parse()
 
-	rand.Seed(time.Now().UnixNano())
-
 	sum := roll.Roll(*diceToRoll, *amountOfDice, *modifier, *atAdvantage, *atDisadvantage)
 
 	fmt.Println(sum)
+
+	//sum would then get passed to the gui as an output for what was rolled
+	//would then loop back and wait for more inputs in the gui
 }
