1e04927853bc2e1c56a9719496a433e4b0f7288d
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2020-04-21 17:05:26 -0400

    Added in two possible returns from the roll function, what was rolled and what the total is

--- roll/roll.go
+++ roll/roll.go
@@ -6,8 +6,15 @@ import (
 	"github.com/FrankKerschbaumer3/go_roller/minmax"
 )
 
-//Roll will take in arguments and output dice totals based on arguments
-func Roll(diceToRoll, amountOfDice, modifier int, atAdvantage, atDisadvantage bool) int {
+// Result struct for the return values
+type Result struct {
+	Rolled []int
+	Sum    int
+}
+
+// Roll will take in arguments and output what was rolled as the first returned output
+// and the total on the second returned output
+func Roll(diceToRoll, amountOfDice, modifier int, atAdvantage, atDisadvantage bool) *Result {
 
 	slice := make([]int, 0)
 
@@ -18,17 +25,25 @@ func Roll(diceToRoll, amountOfDice, modifier int, atAdvantage, atDisadvantage bo
 			slice = append(slice, num)
 		}
 		// Pass ints to Max minmax.go function
+
 		max := minmax.Max(slice)
-		return modifier + max
+		return &Result{
+			Rolled: slice,
+			Sum:    modifier + max,
+		}
 	} else if atDisadvantage == true {
 		amountOfDice = 2
 		for i := 1; i <= amountOfDice; i++ {
 			num := 1 + rand.Intn(diceToRoll)
 			slice = append(slice, num)
 		}
+
 		// Pass ints to Min minmax.go function
 		min := minmax.Min(slice)
-		return modifier + min
+		return &Result{
+			Rolled: slice,
+			Sum:    modifier + min,
+		}
 	} else {
 		sum := 0
 		num := 0
@@ -36,6 +51,9 @@ func Roll(diceToRoll, amountOfDice, modifier int, atAdvantage, atDisadvantage bo
 			num = 1 + rand.Intn(diceToRoll)
 			sum += num
 		}
-		return modifier + sum
+		return &Result{
+			Rolled: []int{sum},
+			Sum:    modifier + sum,
+		}
 	}
 }
