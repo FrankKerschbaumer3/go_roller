8e0bbe30ab058a3ddab4711855f336dc282325b3
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2020-04-21 17:04:23 -0400

    Added in gin for http requests and shifted away from cli gui

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"encoding/binary"
 	math_rand "math/rand"
 
-	"github.com/FrankKerschbaumer3/go_roller/gui"
+	"github.com/FrankKerschbaumer3/go_roller/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 // Creates seed to have random dice rolls
@@ -18,9 +19,11 @@ func init() {
 	math_rand.Seed(int64(binary.LittleEndian.Uint64(b[:])))
 }
 
-//Function that will accept ints and booleans to output a dice total.
 func main() {
-
-	gui.Gui()
-
+	r := gin.Default()
+	r.GET("/ping", controllers.Pong)
+	r.POST("/roll", controllers.NormalRoll)
+	r.POST("/roll/advantage", controllers.AdvantageRoll)
+	r.POST("/roll/disadvantage", controllers.DisadvantageRoll)
+	r.Run(":8080")
 }
