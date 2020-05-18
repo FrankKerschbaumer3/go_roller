cb830125d0313fab467c5b33018035eb3f8e3702
Frank Kerschbaumer <frank.kerschbaumer@slalom.com>
2020-05-18 12:23:05 -0400

    Update roll test. Add Roll all function for multiple rolls

--- roll/roll.go
+++ roll/roll.go
@@ -1,59 +1,87 @@
 package roll
 
 import (
+	"fmt"
 	"math/rand"
 
 	"github.com/FrankKerschbaumer3/go_roller/minmax"
 )
 
+// Input struct for what to roll
+type Input struct {
+	Sides    int
+	Amount   int // Amount to roll
+	Modifier int
+}
+
 // Result struct for the return values
 type Result struct {
+	Sides  int
 	Rolled []int
-	Sum    int
+	Total  int
 }
 
 // Roll will take in arguments and output what was rolled as the first returned output
 // and the total on the second returned output
-func Roll(diceToRoll, amountOfDice, modifier int, atAdvantage, atDisadvantage bool) *Result {
+func Roll(input Input, atAdvantage, atDisadvantage bool) *Result {
 
 	slice := make([]int, 0)
 
 	if atAdvantage == true {
-		amountOfDice = 2
-		for i := 1; i <= amountOfDice; i++ {
-			num := 1 + rand.Intn(diceToRoll)
+		input.Amount = 2
+		for i := 1; i <= input.Amount; i++ {
+			num := 1 + rand.Intn(input.Sides)
 			slice = append(slice, num)
 		}
 		// Pass ints to Max minmax.go function
 
 		max := minmax.Max(slice)
 		return &Result{
+			Sides:  input.Amount,
 			Rolled: slice,
-			Sum:    modifier + max,
+			Total:  input.Modifier + max,
 		}
 	} else if atDisadvantage == true {
-		amountOfDice = 2
-		for i := 1; i <= amountOfDice; i++ {
-			num := 1 + rand.Intn(diceToRoll)
+		input.Amount = 2
+		for i := 1; i <= input.Amount; i++ {
+			num := 1 + rand.Intn(input.Sides)
 			slice = append(slice, num)
 		}
 
 		// Pass ints to Min minmax.go function
 		min := minmax.Min(slice)
 		return &Result{
+			Sides:  input.Amount,
 			Rolled: slice,
-			Sum:    modifier + min,
+			Total:  input.Modifier + min,
 		}
 	} else {
 		sum := 0
 		num := 0
-		for i := 1; i <= amountOfDice; i++ {
-			num = 1 + rand.Intn(diceToRoll)
+		for i := 1; i <= input.Amount; i++ {
+			num = 1 + rand.Intn(input.Sides)
+			slice = append(slice, num)
 			sum += num
 		}
 		return &Result{
-			Rolled: []int{sum},
-			Sum:    modifier + sum,
+			Sides:  input.Amount,
+			Rolled: slice,
+			Total:  input.Modifier + sum,
 		}
 	}
 }
+
+// All lkjshdalfj
+func All(inputs []Input) []*Result {
+
+	slice := make([]*Result, 0)
+
+	for _, input := range inputs {
+		rolls := Roll(input, false, false)
+		slice = append(slice, rolls)
+		fmt.Println(rolls)
+	}
+	return slice
+}
+
+// call roll multiple times with the various dice to roll
--- roll/roll_test.go
+++ roll/roll_test.go
@@ -5,60 +5,57 @@ import (
 	"testing"
 )
 
-var roll1 = []struct {
-	dice         int
-	amount       int
-	modifier     int
+var tests = []struct {
+	input        Input
 	advantage    bool
 	disadvantage bool
-	expected     int
+	sum          int
 }{
-	{20, 1, 5, false, false, 9},
-	{20, 2, 10, true, false, 19},
-	{20, 2, -3, false, true, 1},
-	{8, 8, 6, false, false, 41},
-	{12, 3, -5, false, false, 17},
-	{20, 1, 0, false, false, 4},
-	{6, 0, 5, false, false, 5},
+	{Input{Sides: 20, Amount: 1, Modifier: 5}, false, false, 9},
+	{Input{Sides: 8, Amount: 8, Modifier: 6}, false, false, 41},
+	{Input{Sides: 20, Amount: 2, Modifier: 10}, true, false, 19},
+	{Input{Sides: 20, Amount: 2, Modifier: -3}, false, true, 1},
+	{Input{Sides: 12, Amount: 3, Modifier: -5}, false, false, 17},
+	{Input{Sides: 20, Amount: 1, Modifier: 0}, false, false, 4},
+	{Input{Sides: 6, Amount: 0, Modifier: 5}, false, false, 5},
 }
 
-var roll2 = []struct {
-	dice         int
-	amount       int
-	modifier     int
-	advantage    bool
-	disadvantage bool
-	expected     int
-}{
-	{20, 1, 9, false, false, 22},
-	{20, 2, -4, true, false, 9},
-	{20, 2, 8, false, true, 12},
-	{10, 10, 3, false, false, 67},
-	{12, 4, -2, false, false, 17},
-	{20, 1, 0, false, false, 13},
-	{6, 0, 10, false, false, 10},
-}
-
-func TestRoll1(t *testing.T) {
-	for _, roll := range roll1 {
+func TestRoll(t *testing.T) {
+	for _, expected := range tests {
 		rand.Seed(100)
 		t.Run("Roll", func(t *testing.T) {
-			result := Roll(roll.dice, roll.amount, roll.modifier, roll.advantage, roll.disadvantage)
-			if result != roll.expected {
-				t.Error("Something went wrong :(")
+			result := Roll(expected.input, expected.advantage, expected.disadvantage)
+			if result.Total != expected.sum {
+				t.Error("expected", expected.sum, "but got", result.Total, ".", "Rolled", expected.input.Amount, "d", expected.input.Sides, result.Rolled, "with a modifier", expected.input.Modifier)
 			}
 		})
 	}
 }
 
-func TestRoll2(t *testing.T) {
-	for _, roll := range roll2 {
-		rand.Seed(666)
-		t.Run("Roll", func(t *testing.T) {
-			result := Roll(roll.dice, roll.amount, roll.modifier, roll.advantage, roll.disadvantage)
-			if result != roll.expected {
-				t.Error("Something went wrong :(")
-			}
-		})
-	}
+var test2 = []struct {
+	input        Input
+	advantage    bool
+	disadvantage bool
+	sum          int
+}{
+	{Input{Sides: 20, Amount: 1, Modifier: 5}, false, false, 9},
+	{Input{Sides: 8, Amount: 8, Modifier: 6}, false, false, 34},
+}
+
+func TestAll(t *testing.T) {
+	rand.Seed(100)
+	t.Run("Roll", func(t *testing.T) {
+		inputs := []Input{test2[0].input, test2[1].input}
+		result := All(inputs)
+		if result[0].Total != test2[0].sum {
+			expected := test2[0]
+			result := result[0]
+			t.Error("Roll1 expected", expected.sum, "but got", result.Total, ".", "Rolled", expected.input.Amount, "d", expected.input.Sides, result.Rolled, "with a modifier", expected.input.Modifier)
+		}
+		if result[1].Total != test2[1].sum {
+			expected := test2[1]
+			result := result[1]
+			t.Error("Roll2 expected", expected.sum, "but got", result.Total, ".", "Rolled", expected.input.Amount, "d", expected.input.Sides, result.Rolled, "with a modifier", expected.input.Modifier)
+		}
+	})
 }
